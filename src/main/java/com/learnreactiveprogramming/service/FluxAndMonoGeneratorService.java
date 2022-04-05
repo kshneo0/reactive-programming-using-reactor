8f7f7f957ef28fac959d02b9341e34aca626ab4a
kshneo0 <kshneo@gmail.com>
2022-04-05 11:26:55 +0900

    25. Handling empty data using defaultIfEmpty and switchIfEmpty() Operators

--- src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -80,11 +80,31 @@ public class FluxAndMonoGeneratorService {
 	public Flux<String> namesFlux_transform(int stringLength){
 		//filter the string whose length is greater than 3
 		
-		Function<Flux<String>, Flux<String>> filtermap =  name -> 
+		Function<Flux<String>, Flux<String>> filterMap =  name -> 
 			name.map(String::toUpperCase).filter( s -> s.length() > stringLength);
+			
 		return Flux.fromIterable(List.of("alex","ben","chloe"))
-				.transform(filtermap)
-				.flatMap(s -> splitString(s)) // ALEX, CHLOE -> A, L, E, X, C, H, L, O, E 
+				.transform(filterMap)
+				.flatMap(s -> splitString(s)) // ALEX, CHLOE -> A, L, E, X, C, H, L, O, E
+				.defaultIfEmpty("default")
+				.log();	//db or a remote service call
+	}
+	
+	public Flux<String> namesFlux_transform_switchIfEmpty(int stringLength){
+		//filter the string whose length is greater than 3
+		
+		Function<Flux<String>, Flux<String>> filterMap =  name -> name
+				.map(String::toUpperCase)
+				.filter( s -> s.length() > stringLength)
+				.flatMap(s -> splitString(s));
+		
+		var defaultFlux = Flux.just("default")
+		.transform(filterMap);	//"D", "E", "F", "A", "U", "L", "T"
+		
+		return Flux.fromIterable(List.of("alex","ben","chloe"))
+				.transform(filterMap)
+				.flatMap(s -> splitString(s)) // ALEX, CHLOE -> A, L, E, X, C, H, L, O, E
+				.switchIfEmpty(defaultFlux)
 				.log();	//db or a remote service call
 	}
 	
