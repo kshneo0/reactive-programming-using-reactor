b98ac881209b661003f17f3533b2b59acd661a4d
kshneo0 <kshneo@gmail.com>
2022-04-07 08:43:06 +0900

    56. Handling Backpressure using onBackpressureBuffer()

--- src/test/java/com/learnreactiveprogramming/BackpressureTest.java
+++ src/test/java/com/learnreactiveprogramming/BackpressureTest.java
@@ -10,6 +10,7 @@ import org.reactivestreams.Subscription;
 
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.BufferOverflowStrategy;
 import reactor.core.publisher.Flux;
 
 @Slf4j
@@ -122,6 +123,52 @@ public class BackpressureTest {
 //				}
 				
 				if(value==2) {
+					hookOnCancel();
+				}
+			}
+			
+			@Override
+			protected void hookOnError(Throwable throwable) {
+				
+				
+			}
+			
+			@Override
+			protected void hookOnCancel() {
+				log.info("Inside OnCancel");
+				latch.countDown();
+			}
+			
+			@Override
+			protected void hookOnComplete() {
+				
+			}
+		});
+		assertTrue(latch.await(5L,TimeUnit.SECONDS));
+	}
+	
+	@Test
+	void testBackPressure_buffer() throws InterruptedException {
+		var numberRange = Flux.range(1, 100).log();
+		
+		CountDownLatch latch = new CountDownLatch(1);
+		numberRange
+		.onBackpressureBuffer(10, i -> {
+			log.info("Last Buffered element is : {}", i);
+		})
+		.subscribe(new BaseSubscriber<Integer> () {
+			@Override
+			protected void hookOnSubscribe(Subscription subscription) {
+				request(1);
+			}
+			
+			@Override
+			protected void hookOnNext(Integer value) {
+				log.info("hookOnNext : {}", value);
+				
+				if(value<50) {
+					request(1);
+				} else {
 					hookOnCancel();
 				}
 			}
