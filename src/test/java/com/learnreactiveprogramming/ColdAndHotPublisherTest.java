41f2f4f01ff0eeb4dbf184c9bfba16fcff133486
kshneo0 <kshneo@gmail.com>
2022-04-07 09:45:25 +0900

    64. Hot Streams - ConnectableFlux using autoConnect and RefCount

--- src/test/java/com/learnreactiveprogramming/ColdAndHotPublisherTest.java
+++ src/test/java/com/learnreactiveprogramming/ColdAndHotPublisherTest.java
@@ -41,4 +41,49 @@ public class ColdAndHotPublisherTest {
 			
 		
 	}
+	
+	@Test
+	void hotPublisherTest_autoConnect() {
+		
+		var flux = Flux.range(1,10)
+				.delayElements(Duration.ofSeconds(1));
+		
+		Flux<Integer> hotSource = flux.publish().autoConnect(2);
+		
+		hotSource.subscribe(i -> System.out.println("Subscriber 1 : " + i));
+		delay(2000);
+		hotSource.subscribe(i -> System.out.println("Subscriber 2 : " + i));
+		System.err.println("Two Subscribers are connected");
+		delay(2000);
+		hotSource.subscribe(i -> System.out.println("Subscriber 3 : " + i));
+		delay(10000);
+			
+		
+	}
+
+	@Test
+	void hotPublisherTest_refCount() {
+		
+		var flux = Flux.range(1,10)
+				.delayElements(Duration.ofSeconds(1))
+				.doOnCancel( ()->{
+					System.out.println("Received Cancel Signal");
+				});
+		
+		Flux<Integer> hotSource = flux.publish().refCount(2);
+		
+		var disposable = hotSource.subscribe(i -> System.out.println("Subscriber 1 : " + i));
+		delay(2000);
+		var disposable1 = hotSource.subscribe(i -> System.out.println("Subscriber 2 : " + i));
+		System.err.println("Two Subscribers are connected");
+		delay(2000);
+		disposable.dispose();
+		disposable1.dispose();
+		hotSource.subscribe(i -> System.out.println("Subscriber 3 : " + i));
+		delay(2000);
+		hotSource.subscribe(i -> System.out.println("Subscriber 4 : " + i));
+		delay(10000);
+		
+		
+	}
 }
