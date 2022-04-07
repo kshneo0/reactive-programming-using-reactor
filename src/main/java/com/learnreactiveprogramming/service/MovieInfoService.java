2b82c1deefca2ad13d9726f66f245fe246fc85d3
kshneo0 <kshneo@gmail.com>
2022-04-07 10:24:39 +0900

    67. Build the non-blocking MovieInfoService RestClient

--- src/main/java/com/learnreactiveprogramming/service/MovieInfoService.java
+++ src/main/java/com/learnreactiveprogramming/service/MovieInfoService.java
@@ -1,17 +1,35 @@
 package com.learnreactiveprogramming.service;
 
-import com.learnreactiveprogramming.domain.MovieInfo;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+import static com.learnreactiveprogramming.util.CommonUtil.delay;
 
 import java.time.LocalDate;
 import java.util.List;
 
-import static com.learnreactiveprogramming.util.CommonUtil.delay;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.learnreactiveprogramming.domain.MovieInfo;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public class MovieInfoService {
 
-    public  Flux<MovieInfo> retrieveMoviesFlux(){
+	private WebClient webClient;
+	
+    public MovieInfoService(WebClient webClient) {
+		this.webClient = webClient;
+	}
+    public MovieInfoService() {
+    	
+    }
+    
+    public Flux<MovieInfo> retrieveAllMovieInfo_RestClient(){
+    	return webClient.get().uri("/v1/movie_infos")
+    		.retrieve()
+    		.bodyToFlux(MovieInfo.class);
+    }
+
+	public  Flux<MovieInfo> retrieveMoviesFlux(){
 
         var movieInfoList = List.of(new MovieInfo(1l,100l, "Batman Begins", 2005, List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15")),
                 new MovieInfo(2l, 101l,"The Dark Knight", 2008, List.of("Christian Bale", "HeathLedger"), LocalDate.parse("2008-07-18")),
